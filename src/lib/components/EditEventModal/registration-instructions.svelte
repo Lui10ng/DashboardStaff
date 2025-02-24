3528d1a44573c312693ad8edc12dc9e80ca2ec6a
Evami-Suarez <suarezevami20@gmail.com>
2025-02-24 15:38:57 +0800

    Created and Added responsive edit event modal

---
+++ src/lib/components/EditEventModal/registration-instructions.svelte
@@ -0,0 +1,5 @@
+<div class="p-4 h-130">
+    <h2 class="text-xl font-semibold">Registration Instructions</h2>
+    <p>Instructions for registration go here.</p>
+  </div>
+  
---
+++ src/lib/components/EditEventModal/visuals.svelte
@@ -0,0 +1,4 @@
+<div class="p-4 h-130">
+    <h2 class="text-xl font-semibold">Visual</h2>
+    <p>Content for the visual section goes here.</p>
+  </div>
