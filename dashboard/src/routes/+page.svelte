ddaab919a3157e78cf90227de8610c5f03de1759
harold <rhey.pisos@gmail.com>
2025-03-03 15:47:05 +0800

    routing

--- dashboard/src/routes/+page.svelte
+++ dashboard/src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="flex h-screen flex-col items-center justify-center">
+	<a href="/form-builder">Form Builder</a>
+</div>
