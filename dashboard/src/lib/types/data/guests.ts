88bc72c65a7e8fdf688d6aed3dc3a5f952931f0d
KENNETHSAYAN <kenkensayan143@gmail.com>
2025-03-05 12:07:19 +0800

    registrants list , pagination

---
+++ dashboard/src/lib/types/data/guests.ts
@@ -0,0 +1,268 @@
+export interface Guest {
+    id: string;
+    name: string;
+    email: string;
+    registrationDate: string;
+    status: 'registered' | 'pending';
+    avatar: string;
+  }
+  
+  export const guests: Guest[] = [
+    {
+      id: '1',
+      name: 'Julia Echlin',
+      email: 'julia@email.com',
+      registrationDate: '09 Jan 2024, 8:04 AM',
+      status: 'registered',
+      avatar: 'https://ui-avatars.com/api/?name=Julia+Echlin&background=ef4444&color=fff'
+    },
+    {
+      id: '2',
+      name: 'Aaron Dave Taglinao',
+      email: 'aaron@email.com',
+      registrationDate: '10 Jan 2024, 10:15 AM',
+      status: 'registered',
+      avatar: 'https://ui-avatars.com/api/?name=Aaron+Dave&background=eab308&color=fff'
+    },
+    {
+      id: '3',
+      name: 'Ericke Gallardo',
+      email: 'erick@email.com',
+      registrationDate: '12 Jan 2024, 11:00 PM',
+      status: 'pending',
+      avatar: 'https://ui-avatars.com/api/?name=Ericka+Gallardo&background=ef4444&color=fff'
+    },
+    {
+      id: '4',
+      name: 'Kenneth Sayan',
+      email: 'kennethsayan@email.com',
+      registrationDate: '12 Jan 2024, 11:00 PM',
+      status: 'registered',
+      avatar: 'https://ui-avatars.com/api/?name=Kenneth+Sayan&background=ef4444&color=fff'
+    },
+    {
+      id: '5',
+      name: 'Jie Clark Terec',
+      email: 'Jieclark@email.com',
+      registrationDate: '12 Jan 2024, 11:00 PM',
+      status: 'registered',
+      avatar: 'https://ui-avatars.com/api/?name=Kenneth+Sayan&background=ef4444&color=fff'
+    },
+    {
+      id: '6',
+      name: 'Harold Pisos',
+      email: 'Haroldpisos@email.com',
+      registrationDate: '12 Jan 2024, 11:00 PM',
+      status: 'registered',
+      avatar: 'https://ui-avatars.com/api/?name=Kenneth+Sayan&background=ef4444&color=fff'
+    },
+    {
+      id: '7',
+      name: 'Harold Pisos',
+      email: 'Haroldpisos@email.com',
+      registrationDate: '12 Jan 2024, 11:00 PM',
+      status: 'registered',
+      avatar: 'https://ui-avatars.com/api/?name=Kenneth+Sayan&background=ef4444&color=fff'
+    },
+    {
+      id: '8',
+      name: 'Harold Pisos',
+      email: 'Haroldpisos@email.com',
+      registrationDate: '12 Jan 2024, 11:00 PM',
+      status: 'registered',
+      avatar: 'https://ui-avatars.com/api/?name=Kenneth+Sayan&background=ef4444&color=fff'
+    },
+    {
+      id: '9',
+      name: 'Harold Pisos',
+      email: 'Haroldpisos@email.com',
+      registrationDate: '12 Jan 2024, 11:00 PM',
+      status: 'registered',
+      avatar: 'https://ui-avatars.com/api/?name=Kenneth+Sayan&background=ef4444&color=fff'
+    },
+    {
+      id: '10',
+      name: 'Harold Pisos',
+      email: 'Haroldpisos@email.com',
+      registrationDate: '12 Jan 2024, 11:00 PM',
+      status: 'registered',
+      avatar: 'https://ui-avatars.com/api/?name=Kenneth+Sayan&background=ef4444&color=fff'
+    },
+    {
+      id: '11',
+      name: 'Harold Pisos',
+      email: 'Haroldpisos@email.com',
+      registrationDate: '12 Jan 2024, 11:00 PM',
+      status: 'registered',
+      avatar: 'https://ui-avatars.com/api/?name=Kenneth+Sayan&background=ef4444&color=fff'
+    },
+    {
+      id: '12',
+      name: 'Harold Pisos',
+      email: 'Haroldpisos@email.com',
+      registrationDate: '12 Jan 2024, 11:00 PM',
+      status: 'registered',
+      avatar: 'https://ui-avatars.com/api/?name=Kenneth+Sayan&background=ef4444&color=fff'
+    },
+    {
+      id: '13',
+      name: 'Harold Pisos',
+      email: 'Haroldpisos@email.com',
+      registrationDate: '12 Jan 2024, 11:00 PM',
+      status: 'registered',
+      avatar: 'https://ui-avatars.com/api/?name=Kenneth+Sayan&background=ef4444&color=fff'
+    },
+    {
+      id: '14',
+      name: 'Harold Pisos',
+      email: 'Haroldpisos@email.com',
+      registrationDate: '12 Jan 2024, 11:00 PM',
+      status: 'registered',
+      avatar: 'https://ui-avatars.com/api/?name=Kenneth+Sayan&background=ef4444&color=fff'
+    },
+    {
+      id: '15',
+      name: 'Harold Pisos',
+      email: 'Haroldpisos@email.com',
+      registrationDate: '12 Jan 2024, 11:00 PM',
+      status: 'registered',
+      avatar: 'https://ui-avatars.com/api/?name=Kenneth+Sayan&background=ef4444&color=fff'
+    },
+    {
+      id: '16',
+      name: 'Harold Pisos',
+      email: 'Haroldpisos@email.com',
+      registrationDate: '12 Jan 2024, 11:00 PM',
+      status: 'registered',
+      avatar: 'https://ui-avatars.com/api/?name=Kenneth+Sayan&background=ef4444&color=fff'
+    },
+    {
+      id: '17',
+      name: 'Harold Pisos',
+      email: 'Haroldpisos@email.com',
+      registrationDate: '12 Jan 2024, 11:00 PM',
+      status: 'registered',
+      avatar: 'https://ui-avatars.com/api/?name=Kenneth+Sayan&background=ef4444&color=fff'
+    },
+    {
+      id: '18',
+      name: 'Harold Pisos',
+      email: 'Haroldpisos@email.com',
+      registrationDate: '12 Jan 2024, 11:00 PM',
+      status: 'registered',
+      avatar: 'https://ui-avatars.com/api/?name=Kenneth+Sayan&background=ef4444&color=fff'
+    },
+    {
+      id: '19',
+      name: 'Harold Pisos',
+      email: 'Haroldpisos@email.com',
+      registrationDate: '12 Jan 2024, 11:00 PM',
+      status: 'registered',
+      avatar: 'https://ui-avatars.com/api/?name=Kenneth+Sayan&background=ef4444&color=fff'
+    },
+    {
+      id: '20',
+      name: 'Harold Pisos',
+      email: 'Haroldpisos@email.com',
+      registrationDate: '12 Jan 2024, 11:00 PM',
+      status: 'registered',
+      avatar: 'https://ui-avatars.com/api/?name=Kenneth+Sayan&background=ef4444&color=fff'
+    },
+    {
+      id: '21',
+      name: 'Harold Pisos',
+      email: 'Haroldpisos@email.com',
+      registrationDate: '12 Jan 2024, 11:00 PM',
+      status: 'registered',
+      avatar: 'https://ui-avatars.com/api/?name=Kenneth+Sayan&background=ef4444&color=fff'
+    },
+    {
+      id: '22',
+      name: 'Harold Pisos',
+      email: 'Haroldpisos@email.com',
+      registrationDate: '12 Jan 2024, 11:00 PM',
+      status: 'registered',
+      avatar: 'https://ui-avatars.com/api/?name=Kenneth+Sayan&background=ef4444&color=fff'
+    },
+    {
+      id: '23',
+      name: 'Harold Pisos',
+      email: 'Haroldpisos@email.com',
+      registrationDate: '12 Jan 2024, 11:00 PM',
+      status: 'registered',
+      avatar: 'https://ui-avatars.com/api/?name=Kenneth+Sayan&background=ef4444&color=fff'
+    },
+    {
+      id: '24',
+      name: 'Harold Pisos',
+      email: 'Haroldpisos@email.com',
+      registrationDate: '12 Jan 2024, 11:00 PM',
+      status: 'registered',
+      avatar: 'https://ui-avatars.com/api/?name=Kenneth+Sayan&background=ef4444&color=fff'
+    },
+    {
+      id: '25',
+      name: 'Harold Pisos',
+      email: 'Haroldpisos@email.com',
+      registrationDate: '12 Jan 2024, 11:00 PM',
+      status: 'registered',
+      avatar: 'https://ui-avatars.com/api/?name=Kenneth+Sayan&background=ef4444&color=fff'
+    },
+    {
+      id: '26',
+      name: 'Harold Pisos',
+      email: 'Haroldpisos@email.com',
+      registrationDate: '12 Jan 2024, 11:00 PM',
+      status: 'registered',
+      avatar: 'https://ui-avatars.com/api/?name=Kenneth+Sayan&background=ef4444&color=fff'
+    },
+    {
+      id: '27',
+      name: 'Harold Pisos',
+      email: 'Haroldpisos@email.com',
+      registrationDate: '12 Jan 2024, 11:00 PM',
+      status: 'registered',
+      avatar: 'https://ui-avatars.com/api/?name=Kenneth+Sayan&background=ef4444&color=fff'
+    },
+    {
+      id: '28',
+      name: 'Harold Pisos',
+      email: 'Haroldpisos@email.com',
+      registrationDate: '12 Jan 2024, 11:00 PM',
+      status: 'registered',
+      avatar: 'https://ui-avatars.com/api/?name=Kenneth+Sayan&background=ef4444&color=fff'
+    },
+    {
+      id: '29',
+      name: 'Harold Pisos',
+      email: 'Haroldpisos@email.com',
+      registrationDate: '12 Jan 2024, 11:00 PM',
+      status: 'registered',
+      avatar: 'https://ui-avatars.com/api/?name=Kenneth+Sayan&background=ef4444&color=fff'
+    },
+    {
+      id: '30',
+      name: 'Harold Pisos',
+      email: 'Haroldpisos@email.com',
+      registrationDate: '12 Jan 2024, 11:00 PM',
+      status: 'registered',
+      avatar: 'https://ui-avatars.com/api/?name=Kenneth+Sayan&background=ef4444&color=fff'
+    },
+    {
+      id: '31',
+      name: 'Harold Pisos',
+      email: 'Haroldpisos@email.com',
+      registrationDate: '12 Jan 2024, 11:00 PM',
+      status: 'registered',
+      avatar: 'https://ui-avatars.com/api/?name=Kenneth+Sayan&background=ef4444&color=fff'
+    },
+    {
+      id: '32',
+      name: 'Harold Pisos',
+      email: 'Haroldpisos@email.com',
+      registrationDate: '12 Jan 2024, 11:00 PM',
+      status: 'registered',
+      avatar: 'https://ui-avatars.com/api/?name=Kenneth+Sayan&background=ef4444&color=fff'
+    }
+
+  ];
