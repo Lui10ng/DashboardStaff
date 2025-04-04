5a3610fdabcbe248236dd2c51a9dbbdc578eca17
aaron <adtagolimot@gmail.com>
2025-04-04 16:46:41 +0800

    [update][registration] - ticket and details

---
+++ v2-registration/src/lib/components/Tooltip.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { Tooltip } from '@skeletonlabs/skeleton-svelte';
+	let data = $props();
+</script>
+
+<Tooltip
+	positioning={{ placement: 'bottom' }}
+	contentBase="rounded-md p-3 max-w-sm text-left italic text-white bg-blue-500"
+	openDelay={200}
+	arrow
+>
+	{#snippet trigger()}{data.text}{/snippet}
+	{#snippet content()}
+		{data.content}{/snippet}
+</Tooltip>
