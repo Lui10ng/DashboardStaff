1d5dbfcf0d15e332c9e310863fc8755af7fda795
Evami-Suarez <suarezevami20@gmail.com>
2025-04-15 17:10:21 +0800

    add fontawesome icons in progress

--- v2-dashboard/src/lib/components/EditEventModal/EditEventModal.svelte
+++ v2-dashboard/src/lib/components/EditEventModal/EditEventModal.svelte
@@ -178,7 +178,8 @@
           
           <!-- Wrapper that changes background on focus -->
           <div class="mt-1 flex items-center rounded-md bg-[#F7F8FA] px-3 py-2 group focus-within:bg-[#e9ecf3]">
-            <img src="/icons/globe.png" alt="Globe Icon" class="h-4 w-4 text-gray-500" />
+            <!-- <img src="/icons/globe.png" alt="Globe Icon" class="h-4 w-4 text-gray-500" /> -->
+            <i class="fa-solid fa-globe h-4 w-4 "></i>
             
             <!-- Input Field -->
             <input
@@ -198,11 +199,8 @@
                   <div>
                     <label for="event-address" class="block text-sm font-medium text-gray-700">Event address</label>
                     <div class="relative mt-1">
-                      <span class="absolute inset-y-0 left-3 flex items-center text-gray-500">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 22s8-9.166 8-13a8 8 0 10-16 0c0 3.834 8 13 8 13z" />
-                          <circle cx="12" cy="9" r="3" stroke="currentColor" stroke-width="2" fill="none" />
-                        </svg>
+                      <span class="absolute inset-y-0 left-3 flex items-center ">
+                        <i class="fa-solid fa-location-dot h-4 w-4"></i>
                       </span>
                       <input
                         Name="Event Address"
@@ -435,14 +433,14 @@
                     <label class="pb-2 block text-sm text-black pb-2 font-small">Email Address</label>
                     <div class="relative">
                       <input type="email" name="Email" placeholder="Enter email" class="w-full p-2 pl-10 rounded-md bg-[#F7F8FA] focus:outline-none focus:bg-[#e9ecf3]">
-                      <img src="/icons/icons8-email-50.png" alt="Email" class="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-500">
+                      <i class="fa-regular fa-envelope absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 "></i>
                     </div>
                   </div>
                   <div>
                     <label class="pb-2 block text-sm text-black pb-2 font-small">Mobile Number</label>
                     <div class="relative">
                       <input name="Number" type="tel" placeholder="Enter mobile number" class="w-full p-2 pl-10 rounded-md bg-[#F7F8FA] focus:outline-none focus:bg-[#e9ecf3]">
-                      <img src="/icons/icons8-phone-50.png" alt="Phone" class="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-500">
+                      <i class="fa-regular fa-phone absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5"></i>
                     </div>
                   </div>
                 </div>
