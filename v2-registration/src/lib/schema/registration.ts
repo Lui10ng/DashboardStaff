54c262fb944083226a03b2aa71624f1cbc83db74
aaron <adtagolimot@gmail.com>
2025-04-21 11:53:15 +0800

    feat - v2 payload registration and dashboard

--- v2-registration/src/lib/schema/registration.ts
+++ v2-registration/src/lib/schema/registration.ts
@@ -4,15 +4,13 @@ export const registration = (fields: any): any => {
 	const z_object_fields: any = {};
 	for (const field of fields) {
 		if (field.fieldType == 'email') {
-			z_object_fields[field.name] = z.string().email();
-			continue;
-		}
-		if (field.fieldType == 'checkbox') {
-			z_object_fields[field.name] = z.boolean();
+			z_object_fields[field.id] = z.string().email();
 			continue;
 		}
 
-		z_object_fields[field.name] = z.string().min(1, `${field.label} should not be empty!`);
+		field.required
+			? (z_object_fields[field.id] = z.string().min(1, `${field.label} should not be empty!`))
+			: (z_object_fields[field.id] = z.string());
 	}
 	z_object_fields['payment'] = z.any();
 	return z.object({
--- v2-registration/src/lib/types/index.ts
+++ v2-registration/src/lib/types/index.ts
@@ -3,3 +3,36 @@ export type TicketDetail = {
 	price: number;
 	color: string;
 };
+
+export type HttpMethod = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH';
+
+export interface RequestOptions {
+	body?: unknown;
+	params?: Record<string, string> | URLSearchParams;
+	fetchInstance?: typeof fetch;
+	token?: string;
+	headers?: Record<string, string>;
+}
+
+export interface ApiClientOptions extends Omit<RequestOptions, 'params' | 'body'> {}
+
+export interface ApiClient {
+	get: <T = unknown>(
+		path: string,
+		params?: Record<string, string> | URLSearchParams | undefined,
+		options?: ApiClientOptions
+	) => Promise<T>;
+
+	post: <T = unknown>(path: string, body: unknown, options?: ApiClientOptions) => Promise<T>;
+
+	patch: <T = unknown>(path: string, body: unknown, options?: ApiClientOptions) => Promise<T>;
+
+	del: <T = unknown>(path: string, options?: ApiClientOptions) => Promise<T>;
+}
+
+export interface PayloadError {
+	message?: string;
+	errors?: { field: string; message: string }[];
+	// Add other potential properties if known
+	[key: string]: any; // Allow other properties
+}
