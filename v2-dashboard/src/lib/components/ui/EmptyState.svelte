4016bb3383196df384286b8545bcbf5f207e0a16
clarkscode <jieclarkm@gmail.com>
2025-05-08 10:46:05 +0800

    update: empty svg

--- v2-dashboard/src/lib/components/ui/EmptyState.svelte
+++ v2-dashboard/src/lib/components/ui/EmptyState.svelte
@@ -1,26 +1,22 @@
 <script lang="ts">
-    import Button from '$lib/components/ui/Button.svelte';
-    export let title = '';
-    export let description = '';
-    export let buttonLabel = '';
-    export let buttonIcon = '';
-    export let onClick: () => void;
+	import Button from '$lib/components/ui/Button.svelte';
+	export let title = '';
+	export let description = '';
+	export let buttonLabel = '';
+	export let buttonIcon = '';
+	export let onClick: () => void;
 </script>
 
-<div class="flex flex-col items-center justify-center py-16 px-4 text-center">
-    <img 
-        src="/images/empty.svg" 
-        alt="No items" 
-        class="mb-6 w-48 h-64 text-gray-400"
-    />
-    <h3 class="prose mb-2 text-lg font-medium text-gray-900">{title}</h3>
-    <p class="prose mb-6 text-sm text-gray-500">
-        {description}
-    </p>
-    <Button
-        label={buttonLabel}
-        icon={buttonIcon}
-        className="bg-primary text-white rounded-lg px-4 py-2"
-        {onClick}
-    />
+<div class="flex flex-col items-center justify-center px-4 py-16 text-center">
+	<img src="/images/empty.svg" alt="No items" class="mb-6 h-64 w-48 text-gray-400" />
+	<h3 class="prose mb-2 text-lg font-medium text-gray-900">{title}</h3>
+	<p class="prose mb-6 text-sm text-gray-500">
+		{description}
+	</p>
+	<Button
+		label={buttonLabel}
+		icon={buttonIcon}
+		className="bg-primary text-white rounded-lg px-4 py-2"
+		{onClick}
+	/>
 </div>
